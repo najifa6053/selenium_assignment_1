f0913c2e215c41657fec069e95af02c54901f629
najifa6053 <esha15-6053@diu.edu.bd>
2025-09-21 18:13:21 +0600

    ID

---
+++ test.py
@@ -0,0 +1,30 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager    
+
+
+def setup_driver():
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    driver.maximize_window()
+    return driver
+
+
+#Using ID
+def test_id(driver):
+    driver.get("https://www.saucedemo.com/")
+    time.sleep(2)
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    time.sleep(2)
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    time.sleep(2)
+    driver.find_element(By.ID, "login-button").click()
+    time.sleep(2)
+    driver.quit()
+    
+    
+if __name__ == "__main__":
+    driver = setup_driver()
+    test_id(driver)
